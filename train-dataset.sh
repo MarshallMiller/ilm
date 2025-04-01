5e5a29c3095b8668e69a9ef286e915180921cbce
MarshallMiller <joshmiller@hotmail.com>
2025-03-31 23:46:55 -0400

    Create train-dataset.sh

---
+++ train-dataset.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+usage() {
+  echo "usage: $0 DATASET"
+}
+
+if [ $# -ne 2 ]; then
+  usage
+  exit 1
+fi
+
+DATASET=$1
+TRAIN_DIR=train
+EXAMPLES_DIR=data/char_masks/${DATASET}
+
+python train_ilm.py \
+	experiment_${DATASET} \
+	${TRAIN_DIR} \
+	${EXAMPLES_DIR} \
+	--seed 0 \
+	--train_examples_tag train \
+	--eval_examples_tag valid \
+	--eval_max_num_examples 512
